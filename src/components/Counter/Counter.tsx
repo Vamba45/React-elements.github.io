5f5df9688ac67b458743991cc138445c53f94c36
Vamba45 <vambafool@bk.ru>
2023-12-11 23:18:24 +0300

    finish count

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,13 +1,24 @@
+import React from 'react';
 import './Counter.scss';
 
 function Counter() {
+    const[count, setCount] = React.useState(0);
+
+    function btnIncrClick() {
+        setCount(count + 1)
+    }
+
+    function btnDecrClick() {
+        setCount(count - 1)
+    }
+
     return (
         <div className="counter">
             <h2>Счётчик:</h2>
-            <h1>0</h1>
+            <h1>{count}</h1>
             <div className="btns">
-                <button className='decr btn'>- Минус</button>
-                <button className='incr btn'>Плюс +</button>
+                <button onClick={btnDecrClick} className='decr btn'>- Минус</button>
+                <button onClick={btnIncrClick} className='incr btn'>Плюс +</button>
             </div>
         </div>
     )
