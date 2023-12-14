68809d293b20db8a7c96e64e3b94b12a6ed52069
Vamba45 <vambafool@bk.ru>
2023-12-14 20:49:03 +0300

    finish quiz

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -24,37 +24,55 @@ const questions = [
     },
 ];
 
-function Result() {
-    <div className="result">
-        <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-        <h2>Вы отгадали 3 ответа из 10</h2>
-        <button>Попробовать снова</button>
-    </div>
+function Result({ correct, setStep, setCorrect }) {
+    return  (<div className="result">
+                <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+                <h2>Из {questions.length} вопросов верно вы ответили на {correct}</h2>
+                <button onClick={
+                    () => {
+                        setStep(0);
+                        setCorrect(0);
+                    }
+                }>Попробовать снова</button>
+            </div>)
 }
 
 export default function Quiz() {
     const[step, setStep] = React.useState(0);
+    const[correct, setCorrect] = React.useState(0);
     const question = questions[step];
 
     const percentage = Math.floor(step / questions.length * 100);
 
     const onClickVariant = (index) => {
         setStep(step + 1);
+
+        if(index === question.correct) {
+            setCorrect(correct + 1)
+        }
     };
 
     return (
         <div className='Quiz'>
-            <div className="progress">
-                <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
-            </div>
-            <h1>{question.title}</h1>
-            <ul>
-                {
-                    question.variants.map((text, index) => (
-                        <li onClick={() => onClickVariant(index)} key={text}>{text}</li>
-                    ))
-                }
-            </ul>
+            {step !== questions.length ? (
+                <>
+                    <div className="progress">
+                        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+                    </div>
+                    <h1>{question.title}</h1>
+                    <ul>
+                        {
+                            question.variants.map((text, index) => (
+                                <li onClick={() => onClickVariant(index)} key={text}>{text}</li>
+                            ))
+                        }
+                    </ul>
+                </>
+            ) : (
+                <Result correct={correct} 
+                        setStep={setStep} 
+                        setCorrect={setCorrect}/>
+            )}
         </div>
     );
 }
